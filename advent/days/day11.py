bc61d777cf55f34722c9416822e7fbe3d53c166b
James T. Allen <james.thomas.allen@gmail.com>
2020-12-12 20:32:40 +0000

    Day 11.

---
+++ advent/days/day11.py
@@ -0,0 +1,69 @@
+from itertools import product
+
+from advent.util import read_data
+
+
+FLOOR = '.'
+EMPTY = 'L'
+OCCUP = '#'
+
+
+def main() -> list[int]:
+    layout = read_data('day11')
+    stable_layout_1 = step_until_stable(layout, 1, 4)
+    n_occupied_1 = count_occupied(stable_layout_1)
+    stable_layout_2 = step_until_stable(layout, 0, 5)
+    n_occupied_2 = count_occupied(stable_layout_2)
+    return [n_occupied_1, n_occupied_2]
+
+
+def step_until_stable(layout: list[str], max_dist: int, thresh_high: int) -> list[str]:
+    while True:
+        next_layout = step(layout, max_dist, thresh_high)
+        if next_layout == layout:
+            return layout
+        layout = next_layout
+
+
+def step(layout: list[str], max_dist: int, thresh_high: int) -> list[str]:
+    n_adjacent = count_visible(layout, max_dist)
+    return update_seats(layout, n_adjacent, thresh_high=thresh_high)
+
+
+def count_visible(layout: list[str], max_distance: int = 0) -> list[list[int]]:
+    if max_distance == 0:
+        max_distance = max([len(layout), len(layout[0])])
+    count = [[0] * len(row) for row in layout]
+    for i, row in enumerate(layout):
+        for j, char in enumerate(row):
+            if char == OCCUP:
+                for dx, dy in product([-1, 0, 1], [-1, 0, 1]):
+                    if dx == dy == 0:
+                        continue
+                    y, x = i, j
+                    for dist in range(1, max_distance+1):
+                        x += dx
+                        y += dy
+                        if x < 0 or y < 0 or x >= len(row) or y >= len(layout):
+                            break
+                        count[y][x] += 1
+                        if layout[y][x] in (OCCUP, EMPTY):
+                            break
+    return count
+
+
+def update_seats(layout: list[str], n_adjacent: list[list[int]],
+                 thresh_low: int = 0, thresh_high: int = 4) -> list[str]:
+    return [
+        ''.join(
+            OCCUP if char == EMPTY and n <= thresh_low else
+            EMPTY if char == OCCUP and n >= thresh_high else
+            char
+            for char, n in zip(row, n_adj_row)
+        )
+        for row, n_adj_row in zip(layout, n_adjacent)
+    ]
+
+
+def count_occupied(layout: list[str]) -> int:
+    return ''.join(layout).count(OCCUP)
