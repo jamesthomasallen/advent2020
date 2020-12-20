ba60790c5e7afa51922a99359a904afadf5f1b1b
James T. Allen <james.thomas.allen@gmail.com>
2020-12-20 13:13:41 +0000

    Day 20 optimisation.

--- advent/days/day20.py
+++ advent/days/day20.py
@@ -24,15 +24,24 @@ def main() -> list[int]:
 
 
 def process_tiles(tiles_raw: list[str]) -> list[dict]:
-    return [process_tile(tile_raw) for tile_raw in tiles_raw]
+    result = [process_tile(tile_raw) for tile_raw in tiles_raw]
+    edges = sum((tile['edges'] for tile in result), [])
+    for tile in result:
+        tile['shared_edges'] = sum(
+            edges.count(edge) > 1 for edge in tile['orientations'][0]['edges'].values()
+        )
+        tile['category'] = {2: 'corner', 3: 'edge', 4: 'middle'}[tile['shared_edges']]
+    return result
 
 
 def process_tile(tile_raw: str) -> dict:
     title, image = tile_raw.split(':\n')
-    return {
+    result = {
         'raw': tile_raw,
         'orientations': catalogue_orientations(image, int(title.removeprefix('Tile '))),
     }
+    result['edges'] = list(set(sum((list(o['edges'].values()) for o in result['orientations']), [])))
+    return result
 
 
 def catalogue_orientations(image: str, index: int) -> list[dict]:
@@ -41,10 +50,12 @@ def catalogue_orientations(image: str, index: int) -> list[dict]:
         rows = orientated.split('\n')
         result.append({
             'image': orientated,
-            'top': rows[0],
-            'bottom': rows[-1],
-            'left': ''.join(r[0] for r in rows),
-            'right': ''.join(r[-1] for r in rows),
+            'edges': {
+                'top': rows[0],
+                'bottom': rows[-1],
+                'left': ''.join(r[0] for r in rows),
+                'right': ''.join(r[-1] for r in rows),
+            },
             'index': index,
         })
     return result
@@ -78,21 +89,28 @@ def find_layout(tiles: list[dict]) -> list[list[dict]]:
 
 def yield_layouts(layout: list[list[Optional[dict]]], tiles: list[dict], i_row: int, i_col: int,
                   ) -> Iterator[list[list[dict]]]:
+    n_row = len(layout)
+    n_col = len(layout[0])
     requirements = {}
     if i_row != 0:
-        requirements['top'] = layout[i_row-1][i_col]['bottom']
+        requirements['top'] = layout[i_row-1][i_col]['edges']['bottom']
     if i_col != 0:
-        requirements['left'] = layout[i_row][i_col-1]['right']
-    if i_col == len(layout)-1:
+        requirements['left'] = layout[i_row][i_col-1]['edges']['right']
+    if i_col == n_col-1:
         next_col = 0
         next_row = i_row + 1
     else:
         next_col = i_col + 1
         next_row = i_row
-    for tile in tiles:
+    order = (
+        ['corner', 'edge', 'middle'] if (i_row in (0, n_row-1) and (i_col in (0, n_col-1))) else
+        ['edge', 'middle', 'corner'] if (i_row in (0, n_row-1) != (i_col in (0, n_col-1))) else
+        ['middle', 'corner', 'edge']
+    )
+    for tile in sorted(tiles, key=lambda t: order.index(t['category'])):
         others = [t for t in tiles if t is not tile]
         for orientation in tile['orientations']:
-            if all(orientation[key] == value for key, value in requirements.items()):
+            if all(orientation['edges'][key] == value for key, value in requirements.items()):
                 new_layout = [
                     row if ir != i_row else
                     [t if ic != i_col else orientation for ic, t in enumerate(row)]
@@ -137,18 +155,20 @@ def count_monsters_orientation(image: str) -> tuple[str, int]:
 
 def prepare_monster_re(image: str) -> tuple[str, str]:
     monster_padded = pad_monster(image)
-    monster_re = '#'.join(
-        '(.{'+str(len(spaces))+'})' if spaces else ''
-        for spaces in monster_padded.split('#')
-    )
-    monster_highlight = ''
-    i = 1
-    for spaces in monster_padded.split('#'):
-        if spaces:
-            monster_highlight = monster_highlight + '\\' + str(i)
-            i += 1
-        monster_highlight = monster_highlight + 'O'
-    monster_highlight = monster_highlight[:-1]
+    monster_re = monster_highlight = ''
+    idx = 1
+    while monster_padded:
+        same_search = re.search(monster_padded[0]+'*', monster_padded)
+        n_same = same_search.end() - same_search.start()
+        if monster_padded.startswith('#'):
+            monster_re += '#' * n_same
+            monster_highlight += 'O' * n_same
+        else:
+            char = '[^\n]' if monster_padded.startswith(' ') else '.'
+            monster_re += '(' + char + '{' + str(n_same) + '})'
+            monster_highlight += '\\' + str(idx)
+            idx += 1
+        monster_padded = monster_padded[n_same:]
     return monster_re, monster_highlight
 
 
@@ -156,7 +176,7 @@ def pad_monster(image: str) -> str:
     image_width = len(image.split('\n')[0])
     monster_rows = MONSTER.split('\n')
     monster_width = len(monster_rows[0])
-    padding = ' ' * (image_width - monster_width + 1)
+    padding = 'p' * (image_width - monster_width + 1)
     monster_padded = ''.join(
         row + padding for row in monster_rows[:-1]
     ) + monster_rows[-1]
